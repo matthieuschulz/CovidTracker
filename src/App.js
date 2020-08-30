787b501a4a1141ba91bc2ab11a456efa46105a42
Matthieu Schulz <m.schulz18@ejm.org>
2020-08-30 02:19:07 +0200

    update

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ function App() {
      ?  `https://disease;sh/v3/covid-19/all`
      : `https://diseases.sh/v3/covid-19/countries/${countryCode}`;
 
+     
      await fetch(url)
      .then(response => response.json())
      .then(data =>{
