8e7fae63fbe0e0dec4cf6306cd0b638ed8541099
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-07 01:14:42 +0200

    update

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   //STATE = How to declare a variable in REACT
   //USEEFFECT = Runs a piece of code based on a given condition
   //async -> sned a request, wait for it, do something with info
+
   
 
   useEffect(() => {
